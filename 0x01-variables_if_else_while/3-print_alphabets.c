93e1944c70053e75d39586e1e7d6f96ccec9df9d
EbisaGirma21 <100440432+EbisaGirma21@users.noreply.github.com>
2022-09-09 19:57:14 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int lch = 122;
-			int ch;
+	int lch = 122;
+	int ch;
 
-				for (ch = 97; ch <= lch; ch++)
-						{
-									putchar (ch);
+	for (ch = 97; ch <= lch; ch++)
+	{
+		putchar (ch);
 
-											if (ch == 122)
-														{
-																		ch = 64;
-																					lch = 90;
-																							}
-												}
-					printf("\n");
-						return (0);
+		if (ch == 122)
+		{
+			ch = 64;
+			lch = 90;
+		}
+	}
+	printf("\n");
+	return (0);
 }
