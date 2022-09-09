660c0e0543d65d5b5a9f21144c9e993c16890242
EbisaGirma21 <100440432+EbisaGirma21@users.noreply.github.com>
2022-09-09 19:57:51 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int c;
+	int c;
 
-			for (c = 97; c <= 122; c++)
-					{
-								putchar (c);
-									}
-				printf("\n");
-					return (0);
+	for (c = 97; c <= 122; c++)
+	{
+		putchar (c);
+	}
+	printf("\n");
+	return (0);
 }
